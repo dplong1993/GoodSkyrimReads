24253328b91df7e7f357c65c57f44fd29dc6c23f
Dillon Long <dplong@crimson.ua.edu>
2020-09-29 16:21:45 -0700

    Deleted a line in App.js that was no long needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, useLocation } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import {useSelector} from 'react-redux';
 
-import UserList from './components/UsersList';
 import Login from './components/auth/Login';
 import NavBar from './components/nav/NavBar';
 import Signup from './components/auth/Signup';
