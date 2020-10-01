573010acde9ec12dd3d7c168db1b3ae4fee4b28f
Dillon Long <dplong@crimson.ua.edu>
2020-10-01 14:37:14 -0700

    Added a few more books to seeder file to make the homepage look more complete.

--- db/seeders/20200930225551-insert-books.js
+++ db/seeders/20200930225551-insert-books.js
@@ -12,6 +12,8 @@ module.exports = {
       r({ title: "16 Accords of Madness, v. VI", description: "A tale of a duel between Sheogorath and Hircine.", author: "Anonymous", coverPhotoUrl: "/images/Book06.png"}),
       r({ title: "16 Accords of Madness, v. IX", description: "Sheogorath and Vaermina strike a bet.", author: "Anonymous", coverPhotoUrl: "/images/Book06.png"}),
       r({ title: "16 Accords of Madness, v. XII", description: "Malacath's dealings with Sheogorath.", author: "Anonymous", coverPhotoUrl: "/images/Book06.png"}),
+      r({ title: "2920, vol 01 - Morning Star", description: "First book in a series of books recounting the events surrounding the Empire during the last year of the First Era.", author: "Carlovac Townway", coverPhotoUrl: "/images/Book04Red.png"}),
+      r({ title: "2920, vol 02 - Sun's Dawn", description: "	Details one of Sotha Sil's lessons, Turala's exile and Molag Bal arriving to Gilverdale, as well as Almalexia discovering the city's destruction.", author: "Carlovac Townway", coverPhotoUrl: "/images/IllusionSkillBook.png"}),
     ]);
   },
 
