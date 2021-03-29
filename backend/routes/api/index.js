12ca4714fd50d688ac96b8379e11c66d0f12a4a8
Dillon Long <dplong@crimson.ua.edu>
2021-03-29 13:10:59 -0700

    Added backend login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
 
-// GET /api/set-token-cookie
-const asyncHandler = require("express-async-handler");
-const { setTokenCookie } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+module.exports = router;
+
+// Testing auth util routes
+// // GET /api/set-token-cookie
+// const asyncHandler = require("express-async-handler");
+// const { setTokenCookie } = require("../../utils/auth.js");
+// const { User } = require("../../db/models");
 // router.get(
 //   "/set-token-cookie",
 //   asyncHandler(async (req, res) => {
