55cfd0d3b4bab53ef69b10ae7c4201cb3188f74e
Dillon Long <dplong@crimson.ua.edu>
2021-03-30 10:24:35 -0500

    Updated SignupFormPage to be a modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import SplashPage from "./components/SplashPage";
@@ -20,9 +19,6 @@ function App() {
         <Switch>
           <Route exact path="/">
             <SplashPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
           </Route>
         </Switch>
       )}
